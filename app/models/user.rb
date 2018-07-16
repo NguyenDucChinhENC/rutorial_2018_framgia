90f9c3e2add5adf6fc1a848a6f7beaae81a471a1
Admin <admin@Ebolaa.local>
2018-07-16 14:50:27 +0700

    chapter 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,38 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.min_pass}
+  validates :password, presence: true, length: {minimum: Settings.min_pass},
+    allow_nil: true
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_token: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_token.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_token: nil
+  end
 
   class << self
     def digest string
